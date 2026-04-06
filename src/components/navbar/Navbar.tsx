a42ccdd833667398079dfa51d1c2e1fcda6a54cb
jrs-neto <jrd.santosneto@gmail.com>
2026-04-06 18:34:19 -0300

    ✨ New Feature: Adiciona sistema de autenticação com contexto e fluxo de usuário.

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,16 +1,36 @@
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom"
+import { AuthContext } from "../../contexts/AuthContext";
+
 function Navbar() {
+  // Objeto responsável por redirecionar o usuário para uma outra rota
+  const navigate = useNavigate();
+
+  // Consumo do Contexto AuthContext (usamos a desestruturação para selecionar apenas o que precisamos)
+  const { handleLogout } = useContext(AuthContext);
+
+  function logout() {
+    handleLogout();
+    alert('O Usuário foi desconectado com sucesso!')
+    navigate('/')
+  }
+
   return (
     <>
       <div className='w-full flex justify-center py-4 bg-indigo-900 text-white'>
 
-        <div className='container flex justify-between text-lg mx-8'>Blog Pessoal
-
+        <div className='container flex justify-between text-lg mx-8'>
+          <Link to='/home' className="text-2xl font-bold">
+            Blog Pessoal
+          </Link>
           <div className='flex gap-4'>
             Postagens
             Temas
             Cadastrar tema
             Perfil
-            Sair
+            <Link to='' onClick={logout} className="hover:underline">
+              Sair
+            </Link>
           </div>
         </div>
       </div>
