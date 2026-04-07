a4ea15a2f62522ac83f7309b9c28616cf546f526
jrs-neto <jrd.santosneto@gmail.com>
2026-04-07 19:09:58 -0300

    ✨ New Feature: Implementar CRUD de temas com formulários, listagem e exclusão.

--- src/pages/cadastro/Cadastro.tsx
+++ src/pages/cadastro/Cadastro.tsx
@@ -144,19 +144,17 @@ function Cadastro() {
           </div>
           <div className="flex justify-around w-full gap-8">
             <button type="reset" className="rounded text-white bg-red-400 hover:bg-red-700 w-1/2 py-2" onClick={retornar}>Cancelar</button>
-            <button type="submit" className="rounded text-white bg-indigo-400 hover:bg-indigo-900 w-1/2 py-2 flex justify-center">{
-              isLoading ?
-
-                <ClipLoader
-                  color="#ffffff"
-                  size={24}
-                />
-
-                :
-
-                <span>Cadastrar</span>
-
-            }</button>
+            <button type="submit" className="rounded text-white bg-indigo-400 hover:bg-indigo-900 w-1/2 py-2 flex justify-center">
+              {
+                isLoading ?
+                  <ClipLoader
+                    color="#ffffff"
+                    size={24}
+                  />
+                  :
+                  <span>Cadastrar</span>
+              }
+            </button>
           </div>
         </form>
       </div>
