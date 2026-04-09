6dd66eab367c43412da5af68ec39c9e7427a16de
jrs-neto <jrd.santosneto@gmail.com>
2026-04-09 17:00:11 -0300

    Estilização e Lógica dos componentes CardPostagem e ListaPostagens

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,5 @@
+import ListaPostagens from "../../components/postagem/listapostagens/ListaPostagens"
+
 function Home() {
   return (
     <>
@@ -30,6 +32,8 @@ function Home() {
 
         </div>
       </div>
+
+      <ListaPostagens />
     </>
   )
 }
