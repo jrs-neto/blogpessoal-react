0bfa6000a841a10787257945898a86c1ea3fb3be
jrs-neto <jrd.santosneto@gmail.com>
2026-04-10 10:33:01 -0300

    Integração da lógica do Componente com o Formulário

--- src/components/postagem/formpostagem/FormPostagem.tsx
+++ src/components/postagem/formpostagem/FormPostagem.tsx
@@ -4,6 +4,7 @@ import type Tema from "../../../models/Tema";
 import type Postagem from "../../../models/Postagem";
 import { AuthContext } from "../../../contexts/AuthContext";
 import { atualizar, buscar, cadastrar } from "../../../services/Service";
+import { ClipLoader } from "react-spinners";
 
 function FormPostagem() {
 
@@ -140,9 +141,11 @@ function FormPostagem() {
 
   return (
     <div className="text-4xl flex flex-col mx-auto items-center">
-      <h1 className="text-4xl text-center my-8">Cadastrar Postagem</h1>
+      <h1 className="text-4xl text-center my-8">
+        {id !== undefined ? 'Editar Postagem' : 'Cadastrar Postagem'}
+      </h1>
 
-      <form className="flex flex-col w-1/2 gap-4">
+      <form className="flex flex-col w-1/2 gap-4" onSubmit={gerarNovaPostagem}>
         <div className="flex flex-col gap-2">
           <label htmlFor="titulo">Título da Postagem</label>
           <input type="text"
@@ -150,27 +153,48 @@ function FormPostagem() {
             name="titulo"
             required
             className="border-2 border-slate-700 rounded p-2"
+            value={postagem.titulo}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
           />
         </div>
+
         <div className="flex flex-col gap-2">
           <label htmlFor="titulo">Texto da Postagem</label>
           <input type="text"
             placeholder="Texto"
             name="texto"
             required
-            className="border-2 border-slate-700 rounded p-2" />
+            className="border-2 border-slate-700 rounded p-2"
+            value={postagem.texto}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+          />
         </div>
+
         <div className="flex flex-col gap-2">
           <p>Tema da Postagem</p>
-          <select name="tema" id="tema" className="border p-2 border-slate-800 rounded">
+          <select name="tema" id="tema" className="border p-2 border-slate-800 rounded"
+            onChange={(e) => buscarTemaPorId(e.currentTarget.value)}
+          >
             <option value="" selected disabled>Selecione um Tema</option>
-            <>
-              <option>tema1</option>
-            </>
+
+            {temas.map((tema) => (
+              <>
+                <option value={tema.id}>{tema.descricao}</option>
+              </>
+            ))}
+
           </select>
         </div>
-        <button type="submit" className="rounded disabled:bg-slate-200 bg-indigo-400 hover:bg-indigo-800 text-white font-bold w-1/2 mx-auto py-2 flex justify-center" >
-          Cadastrar
+
+        <button type="submit" className="rounded disabled:bg-slate-200 bg-indigo-400 hover:bg-indigo-800 text-white font-bold w-1/2 mx-auto py-2 flex justify-center" disabled={carregandoTema}>
+          {isLoading ?
+            <ClipLoader
+              color='#ffffff'
+              size={24}
+            /> :
+            <span>{id === undefined ? 'Cadastrar' : 'Atualizar'}</span>
+          }
+
         </button>
       </form>
     </div>
